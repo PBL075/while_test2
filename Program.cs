787014cc9dfaa087a31a9f72cf7912ecec9faaf3
PBL075 <pavelgorea75@gmail.com>
2026-05-06 15:50:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test_while
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+
+            while (i < 17)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
